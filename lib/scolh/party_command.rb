3a2f2bb410a5e9631578d39f7b9614863c8ff9dc
Dave Vandervort <drvandervort@gmail.com>
2016-09-12 12:02:19 -0400

    Added payment address to party

--- lib/scolh/party_command.rb
+++ lib/scolh/party_command.rb
@@ -3,17 +3,40 @@ module Scolh
   # maybe later it will also be attached to their responsibilities
   # OR even have a hierarchy
   class PartyCommand
-    attr_reader :name
+    attr_reader :name, :payment_address
     
     def initialize(command_line)
-      # are there switches or subcommands that would go with this?
-      # and how do we add address, phone number, etc? 
-      @name = command_line.sub("party", "").strip
+      @name, @payment_address = parse(command_line)
     end
     
     # eventually this will include address and stuff
     def to_s
       @name
+    end
+    
+    def update
+      #not yet implemented
+    end
+    
+    private
+    
+    def parse(line)
+      # are there switches or subcommands that would go with this?
+      # It's going to outgrow this method of finding params soon!
+      l = line.sub("party", "").strip
+      parts = l.split(/has/)
+      name = parts.shift.strip
+
+      unless parts.empty?
+        parts.first =~ /\s*payment\s+address\s+(.*)/
+        
+        # TODO: Verify that payment address is valid.
+        # BUT since format changes depending on blockchain used,
+        # this should be managed by configurable plugin modules.
+        payment_address = $1
+      end
+      
+      [name, payment_address]
     end
   end
 end
--- spec/party_command_spec.rb
+++ spec/party_command_spec.rb
@@ -13,4 +13,10 @@ describe Scolh::PartyCommand do
   it "should give english response to to_s" do
     expect(@p.to_s).to eq(@name)
   end
+  
+  it "should accept payment address at init" do
+    addy = "abcdef123456789"
+    p2 = Scolh::PartyCommand.new "party #{ @name } has payment address #{ addy }"
+    expect(p2.payment_address).to eq(addy)
+  end
 end
