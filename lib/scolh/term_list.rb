7b2b9732d76149ccc18c4f8f0cd9b9513a72a557
Dave Vandervort <drvandervort@gmail.com>
2017-01-23 20:44:59 -0500

    Added ability to search terms list for type and some other field

--- lib/scolh/term_list.rb
+++ lib/scolh/term_list.rb
@@ -13,7 +13,27 @@ module Scolh
     # To be stored or updatde in the list
     def <<(item)
       @list << item
+    end
 
+    # IN: a hash of search terms, one of which MUST be type
+    def find(search)
+      found = []
+      if search.key?(:type)
+        found = @list.select{|i| i.class.name =~ /#{search[:type]}/i}
+        # There may be a secondary type or even more.
+        keys = search.keys - [:type]
+        keys.each do |key|
+          found.select! do |i|
+            i.respond_to?(key) && i.send(key) =~ /#{ search[key] }/i
+          end
+        end
+      end
+
+      found
+    end
+
+    def to_a
+      @list
     end
   end
 end
--- spec/term_list_spec.rb
+++ spec/term_list_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Scolh::TermList do
   before do
     @pc1 = Scolh::PartyCommand.new "party joe has payment address 1FfmbHfnpaZjK4vyi1okTjHHvtN455paPH"
-    @pc2 = Scolh::PartyCommand.new "party harcourt has payment address 1FnpaZjKfmbHf4vyjHHvi1okTtpaPHN455"
+    @pc2 = Scolh::PartyCommand.new "party jim has payment address 1FnpaZjKfmbHf4vyjHHvi1okTtpaPHN455"
+    @pay1 = Scolh::PaymentCommand.new  "pay $35 from joe to jim on 3/21/2099"
     @tl = Scolh::TermList.new
   end
 
@@ -11,5 +12,17 @@ describe Scolh::TermList do
     expect(@tl.list).to be_kind_of(Array)
     @tl << @pc1
     expect(@tl.list.size).to eq(1)
+  end
+
+  it "finds terms by type" do
+    @tl << @pc1 << @pay1
+    results = @tl.find type: 'payment'
+    expect(results).to eq([@pay1])
+  end
+
+  it "finds terms by type and value" do
+    @tl << @pc1 << @pay1 << @pc2
+    results = @tl.find type: 'party', name: 'jim'
+    expect(results).to eq([@pc2])
   end
 end
